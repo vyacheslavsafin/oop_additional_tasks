09cf5e159e9208f2266618758078f9bf38af62af
vyacheslavsafin <safin.vr@mail.ru>
2023-07-31 15:30:21 +0300

    init

--- lesson1/students.py
+++ lesson1/students.py
@@ -11,14 +11,15 @@
 """
 
 
-class ...:
+class Students:
 
-    def __init__(...):
-        ...
+    def __init__(self, name, course):
+        self.name = name
+        self.course = course
 
 
-student_1 = ...
-student_2 = ...
+student_1 = Students('Алиса', 3)
+student_2 = Students('Маргарита', 2)
 
 print(student_1.name, student_1.course)  # Алиса 3
 print(student_2.name, student_2.course)  # Маргарита 2
